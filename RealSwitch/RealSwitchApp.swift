8b3eab8819519ae82d1378c6bed200753e2e2126
Cafe <dachscafe@yahoo.co.jp>
2023-02-19 09:03:33 +0900

    Initial Commit

---
+++ RealSwitch/RealSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealSwitchApp.swift
+//  RealSwitch
+//
+//  Created by Cafe on 2023/02/19.
+//
+
+import SwiftUI
+
+@main
+struct RealSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
